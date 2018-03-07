60788ded8337eb39fa8570bf7712721bd448595c
Konshin Alexander <kon2work@gmail.com>
2018-03-07 16:46:35 +0300

    small fixes

--- src/Platform.php
+++ src/Platform.php
@@ -62,15 +62,20 @@ class Platform extends PlatformWrap
 
 
     /*
-    *   $videos = ['720p' => ['path' => '', 'name' => '']]
-    *   $image = ['path' => '', 'name' => '']
+    *   $videos = ['720p' => ['id' => '', 'path' => '', 'name' => '']]
+    *   $image = ['id' => '', 'path' => '', 'name' => '']
     */
 
     public function setupVideoPlayer($videos, $image = [], $vast = null)
     {
         $uploadedVideos = [];
+        $imageId = 0;
 
         foreach ($videos as $key => $video) {
+            if (isset($video['id'])) {
+                $uploadedVideos[$key] = $video['id'];
+                continue;
+            }
             $name = isset($video['name']) ? $video['name'] : null;
             $videoUploadResult = $this->postObject($video['path'], $name);
             if (!$videoUploadResult) {
@@ -81,11 +86,16 @@ class Platform extends PlatformWrap
         }
 
         if (!empty($image)) {
-            $name = isset($image['name']) ? $image['name'] : null;
-            $imageUploadResult = $this->postObject($image['path'], $name);
-            if (!$imageUploadResult) {
-                $this->error[] = ['error' => "Can't upload file to platform", 'data' => $image['path']];
-                return false;
+            if (isset($image['id'])) {
+                $imageId = $image['id'];
+            } else {
+                $name = isset($image['name']) ? $image['name'] : null;
+                $imageUploadResult = $this->postObject($image['path'], $name);
+                if (!$imageUploadResult) {
+                    $this->error[] = ['error' => "Can't upload file to platform", 'data' => $image['path']];
+                    return false;
+                }
+                $imageId = $imageUploadResult['response']['object']['id'];
             }
         }
 
@@ -97,8 +107,8 @@ class Platform extends PlatformWrap
             ]
         ];
 
-        if (!empty($imageUploadResult)) {
-            $additional['json']['screen_shot_id'] = $imageUploadResult['response']['object']['id'];
+        if (!empty($imageId)) {
+            $additional['json']['screen_shot_id'] = $imageId;
         }
 
         if (!empty($vast)) {
@@ -136,12 +146,12 @@ class Platform extends PlatformWrap
         return $this->sendRequest('PUT', $this->getAccessPointUrl(PlatformType::PLR_ACCESS_PNT, 'PUT', $playerId), $additional);
     }
 
-    public function getVideoTranscodeFormats()
+    public function getVideoTranscoderFormats()
     {
         return $this->sendRequest('GET', $this->getAccessPointUrl(PlatformType::TPS_ACCESS_PNT, 'GET'));
     }
 
-    public function postVideoTranscodeTask($objectId, $presetIds)
+    public function postVideoTranscoderTask($objectId, $presetIds)
     {
         $additional = [
             "json" =>
